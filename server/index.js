8a0e2963a4afd7cb9cf78a592a208636f36d266c
RahulNaktode <rahulnaktode04@gmail.com>
2026-01-11 12:12:36 +0530

    add user signup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connetDB from "./db.js";
+import User from './models/User.js'
 
 dotenv.config();
 
@@ -22,6 +23,64 @@ app.get("/health", (req, res) => {
     return res.json({
         status: "OK"
     })
+});
+
+app.post("/signup", async (req, res) =>{
+    const {name, email, mobile, city, country, password} = req.body;
+
+    const newUser = new User({
+        name, email, mobile, city, country, password
+    });
+
+    if(!name){
+        return res.json({
+            success: false,
+            message: "name is requried",
+            data: null
+        })
+    }
+
+    if(!email){
+        return res.json({
+            success: false,
+            message: "email is requried",
+            data: null
+        })
+    }
+
+    if(!password){
+        return res.json({
+            success: false,
+            message: "password is requried",
+            data: null
+        })
+    }
+
+    const existingUser = await User.findOne({email});
+
+    if(existingUser){
+        return res.json({
+            success: false,
+            message: "Email is already existing",
+            data: null,
+        })
+    }
+
+    try{
+        const saveUser = await newUser.save();
+
+        return res.json({
+            success: true,
+            message: "User register Successfully",
+            data: saveUser
+        })
+    }catch(error){
+        return res.json({
+            success: false,
+            message: `User registration failed: ${error.message}`,
+            data: null,
+        })
+    }
 })
 
 app.listen(PORT , () => {
