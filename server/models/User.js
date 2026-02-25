852b6ebf5ececc63d64e84fd7aca1f8357a2b535
RahulNaktode <rahulnaktode04@gmail.com>
2026-02-25 16:59:48 +0530

    User Schema

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    mobile: {
+        type: String,
+    },
+
+    city: {
+        type: String
+    },
+
+    country: {
+        type: String
+    },
+
+    password: {
+        type: String,
+        required: true,
+    }
+})
