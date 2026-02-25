82f43140ba0f4010db5fc24eb4729c5d4e1f5212
RahulNaktode <rahulnaktode04@gmail.com>
2026-02-25 16:39:47 +0530

    connented to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./db.js";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-const PORT = 8020;
+const PORT = process.env.PORT || 8020;
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to Tiny Tour"
+    })
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "OK"
+    })
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`)
+    console.log(`Server is running on PORT: ${PORT}`);
+    connectDB();
 })
